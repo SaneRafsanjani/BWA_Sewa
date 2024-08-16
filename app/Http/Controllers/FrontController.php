70fa832d05824594041e2fa6b5bf924c2178dba7
Rafsan <47489790+SaneRafsanjani@users.noreply.github.com>
2024-08-16 10:29:15 +0700

    coding halaman detais produk {photo thumbnail belum muncul perlu diperbaiki di video ini}

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Category;
+use App\Models\Brand;
 use App\Models\Product;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class FrontController extends Controller
@@ -19,5 +20,19 @@ class FrontController extends Controller
     public function category(Category $category){
         session()->put('category_id',$category->id);
         return view('front.brands',compact('category'));
+    }
+
+    public function brand(Brand $brand){
+       $category_id = session()->get('category_id');
+       $products = Product::where('brand_id',$brand->id)
+       ->where('category_id',$category_id)
+       ->latest()
+       ->get();
+
+       return view('front.gadgets', compact('brand','products'));
+    }
+
+    public function details(Product $product){
+        return view('front.details',compact('product'));
     }
 }
